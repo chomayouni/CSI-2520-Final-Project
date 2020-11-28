e098e46359c546e2d86f41428cf782d0207c8a44
Adam-Lehnert <74278642+Adam-Lehnert@users.noreply.github.com>
2020-11-28 12:22:08 -0500

    Add files via upload

---
+++ CSI-2520-Final-Project-Adam-Final-Project/CSI-2520-Final-Project-main/CSI 2520 Final Project/script.js
@@ -0,0 +1,9 @@
+// This code will prompt the user to enter thier information and sign them up for the
+
+var name;
+var email;
+
+name = window.prompt("Please enter your name");
+email = window.prompt("Please enter your email");
+
+document.write("<h1>Thank You, " + name + "! Our newsletter will now be sent to " + email + " every month. If you have any further questions, or would like to change your subscription, please reach out to us at: TAOC@gmail.com. Have a nice day!<h1>");
